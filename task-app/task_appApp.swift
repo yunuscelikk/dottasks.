97f6747cdcf0c67b8148487135380a517f11e3e2
Yunus <yunusnotfound@icloud.com>
2026-03-30 22:27:36 +0300

    Initial Commit

---
+++ task-app/task_appApp.swift
@@ -0,0 +1,32 @@
+//
+//  task_appApp.swift
+//  task-app
+//
+//  Created by Yunus Çelik on 3/30/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct task_appApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
